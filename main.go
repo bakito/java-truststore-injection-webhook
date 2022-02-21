748a65b41283b7a66c641f8dab69fdcefe3fc0a7
bakito <github@bakito.ch>
2022-02-21 18:42:59 +0100

    use fixed golangci-lint

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-
-	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
