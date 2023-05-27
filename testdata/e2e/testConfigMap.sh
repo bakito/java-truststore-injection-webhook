e5f15ac5267ba169e86aba6be53b6f378501e589
Marc Brugger <github@bakito.ch>
2023-05-27 11:52:20 +0200

    Update testConfigMap.sh

--- testdata/e2e/testConfigMap.sh
+++ testdata/e2e/testConfigMap.sh
@@ -3,5 +3,5 @@ set -e
 sleep 10
 kubectl apply -f testdata/e2e/cert-configmap.yaml
 echo "Read cacerts"
-kubectl get cm java-certs -o json | jq -r '.binaryData.cacerts'
+kubectl get cm -n java-truststore-injection-webhook java-certs -o json | jq -r '.binaryData.cacerts'
 
