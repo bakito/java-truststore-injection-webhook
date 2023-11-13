14260b4e89451d39d6fb7704ae09d9dc92b82fdc
Marc Brugger <github@bakito.ch>
2023-11-13 08:00:03 +0100

    Update installChart.sh

--- testdata/e2e/installChart.sh
+++ testdata/e2e/installChart.sh
@@ -5,5 +5,7 @@ helm upgrade --install java-truststore-injection-webhook chart \
   --namespace jti \
   --create-namespace \
   -f testdata/e2e/e2e-values.yaml \
-  --atomic
+  --atomic \
+  --wait \
+  --timeout 60
 kubectl get pods -n jti
